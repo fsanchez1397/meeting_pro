ecd25f054792dbd7d4afaaa3f89dcdfca1893ead
fsanchez1397 <felipesanchez10@protonmail.com>
2024-10-19 18:08:45 -0400

    Added livestream of screen

--- src/components/buttons/SelectMedia.tsx
+++ src/components/buttons/SelectMedia.tsx
@@ -1,37 +1,49 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+
 function SelectMedia() {
-  const video = document.querySelector("video");
-  const [devices, setDevices] = useState<MediaDeviceInfo[]>([]);
-  // async function getMediaDevices() {
-  //   try {
-  //     const allDevices = await navigator.mediaDevices.enumerateDevices();
+  const videoRef = useRef<HTMLVideoElement>(null);
 
-  //     setDevices(allDevices)
-  //   } catch (err) {
-  //     console.error('Error accessing media devices:', err);
-  //   }
-  // }
-
-  const captureMedia = () => {
-    navigator.mediaDevices
-      .getDisplayMedia({
-        audio: true,
-        video: {
-          width: 320,
-          height: 240,
-          frameRate: 30,
-        },
-      })
-      .then((stream) => {
-        video.srcObject = stream;
-        video.onloadedmetadata = (e) => video.play();
-      })
-      .catch((e) => console.log(e));
+  const testing = async () => {
+    const constraints = { audio: true, video: true };
+    let chunks = [];
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia(constraints);
+      console.log(stream);
+    } catch (err) {
+      console.log(err);
+    }
   };
+
+  const playMedia = async () => {
+    const stream = await navigator.mediaDevices.getDisplayMedia({
+      audio: true,
+      video: {
+        width: 320,
+        height: 240,
+        frameRate: 30,
+      },
+    });
+
+    if (videoRef.current && stream) {
+      videoRef.current.srcObject = stream;
+      videoRef.current.play();
+    }
+  };
+
+  const pauseMedia = () => {
+    if (videoRef.current) {
+      videoRef.current.pause();
+    }
+  };
+  const saveVideo = () => {};
+
   return (
     <>
-      <button onClick={() => captureMedia()}>Display video of screen</button>
-      <video width="320" height="240" autoPlay></video>
+      <video ref={videoRef} width="320" height="240" autoPlay></video>
+      <button onClick={playMedia}>Record</button>
+      <button onClick={testing}>Click me</button>
+      <button onClick={pauseMedia}>Pause</button>
+      <button onClick={saveVideo}>Save Video</button>
     </>
   );
 }
